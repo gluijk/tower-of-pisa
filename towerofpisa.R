3a82693dcb8e2c31f2fa9ef105f36a6b31621dc0
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2026-01-19 01:33:38 +0100

    Add files via upload

--- towerofpisa.R
+++ towerofpisa.R
@@ -2,14 +2,13 @@
 # www.overfitting.net
 # https://www.overfitting.net/2026/01/distribuciones-normales-en-la-torre-de.html
 
-
 library(Cairo)
 
 
 plot_two_skew_normals_and_sum <- function(
         xi1 = -0.2, xi2 =  0.2,  # location
         omega1 = 1, omega2 = 1,  # width (~stddev)
-        alpha1 = 4, alpha2 = 4,  # skewness (asymmetry)
+        alpha1 = -4, alpha2 = 4,  # skewness (asymmetry)
         n = 5000, labels = FALSE
 ) {
     
@@ -25,8 +24,8 @@ plot_two_skew_normals_and_sum <- function(
     to   <- max(xi1 + 5*omega1, xi2 + 5*omega2)
     x <- seq(from, to, length.out = n)
     
-    y1 <- dskewnorm(x, xi1, omega1, -alpha1)
-    y2 <- dskewnorm(x, xi2, omega2,  alpha2)
+    y1 <- dskewnorm(x, xi1, omega1, alpha1)
+    y2 <- dskewnorm(x, xi2, omega2, alpha2)
     ysum <- y1 + y2
     
 
@@ -38,11 +37,11 @@ plot_two_skew_normals_and_sum <- function(
 }
 
 
-# Adjustment to fit the staircase in the Tower of Pisa
+# Adjustments to fit the staircase in the Tower of Pisa
 CairoPNG("plot_skew_normals.png", width=2000, height=1024)
 plot_two_skew_normals_and_sum(
     xi1 = -3.7, xi2 = 3.7,
     omega1=5, omega2=5,
-    alpha1 = 0.8, alpha2 = 0.8
+    alpha1 = -0.8, alpha2 = 0.8
 )
 dev.off()
